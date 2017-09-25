357d72e33511508d6add02b5d51dabb71848513b
Quantumomics, Inc <31345894+quantumomics@users.noreply.github.com>
2017-09-25 11:45:05 +0530

    committed.

---
+++ index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+
+/*!
+ * ----
+ *  G®
+ * ----
+ * G® — A Next Generation Molecular Storage Environment by Quantumomics, Inc.
+ * ___________________________________________________________________________
+ *
+ * Grunt, http://gruntjs.com/ — The JavaScript Task Runner.
+ * ___________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Quantumømics, Inc.
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * ___________________________________________________________________________
+ *
+ * @date      : 25-Sept-2017
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt-cli
+ * @build     : SEED™ — Umeå
+ *              └────── A Sequømics Product — http://sequomics.com/.
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "G®".
+ * ___________________________________________________________________________
+ */
+
+
+// # Usage: $ node -v
+// # Usage: $ npm -v
+// # Usage: $ grunt -version
