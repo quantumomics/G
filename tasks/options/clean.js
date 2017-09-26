98dcfb6ebb7871d6502151a62f7063b75c28477d
Quantumomics, Inc <31345894+quantumomics@users.noreply.github.com>
2017-09-26 16:34:07 +0530

    committed.

---
+++ tasks/options/clean.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+
+/*!
+ * ----
+ *  G®
+ * ----
+ * G® — A Next Generation Molecule Storage Environment by Quantumømics, Inc.
+ * ___________________________________________________________________________
+ *
+ * Grunt, http://gruntjs.com/ — The JavaScript Task Runner.
+ * ___________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Quantumømics, Inc. [http://quantumomics.com/].
+ * @copyright : Sequømics, Inc. [http://sequomics.com/].
+ * ___________________________________________________________________________
+ *
+ * @date      : 04-Sept-2018
+ * @purpose   : Defining the aliased tasks.
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt-contrib-clean
+ * @build     : SEED™ — Skövde
+ *              └────── A Sequømics Product — http://sequomics.com/.
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "G®".
+ * ___________________________________________________________________________
+ */
+
+// global require: true
+// ‡ Task(s) to be defined.
+// № 
